a83994e1e8bedd3471154caa9ecdd6d4dac8c8bc
BlaineParr <Blaine.parr@hotmail.com>
2015-01-29 17:03:38 -0500

    Created First Swing (GUI) Program

--- COMP1011-Lesson04/src/Program.java
+++ COMP1011-Lesson04/src/Program.java
@@ -1,17 +1,34 @@
+import javax.swing.JOptionPane;
+
 /**
  * 
  * 
  * @author Blaine Parr
  * @version January 29, 2015
  */
-public class Program {
-
-	/**
-	 * @param args
-	 */
+public class Program 
+{
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-
-	} //mathod main ends
+		String firstNumber = "";
+		String secondNumber= "";
+		
+		try
+		{
+			firstNumber = JOptionPane.showInputDialog("Enter First Integer");
+			secondNumber = JOptionPane.showInputDialog("Enter Second Integer");
+			
+			int number1 = Integer.parseInt(firstNumber);
+			int number2 = Integer.parseInt(secondNumber);
+			
+			int sum = number1 + number2;
+			
+			JOptionPane.showMessageDialog(null, "The Sum is " + sum, "Sum of two Integers", JOptionPane.PLAIN_MESSAGE);
+		} //try ends
+		catch(Exception e)
+		{
+			JOptionPane.showMessageDialog(null, "Incorrect Values Entered", "ERROR", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		} //catch ends
+	} //method main ends
 } //class Program ends
