847abdbe443516b3d718a58ee36210a280914157
BlaineParr <Blaine.parr@hotmail.com>
2015-01-29 16:00:06 -0500

    Created Project

---
+++ COMP1011-Lesson04/src/Program.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * 
+ * @author Blaine Parr
+ * @version January 29, 2015
+ */
+public class Program {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	} //mathod main ends
+} //class Program ends
